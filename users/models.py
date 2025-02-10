8e1950232e24361654f321f2c282dfdfce6eed96
MuratbekJ <muratbekj2006@gmail.com>
2025-02-09 20:40:32 -0500

    Create user model + migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+# Custom User model
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    preferred_topics = models.ManyToManyField(Topic, blank=True)
+    user_settings = models.JSONField(default=dict)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
